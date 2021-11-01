28c3cdd093ba162a85772d76a47006f364a4ea12
b08x <rwpannick@gmail.com>
2021-11-01 00:48:23 -0400

    added ansible_package_manager module and role. works with fedora dnf so far

---
+++ ansible/plugins/modules/pkg_manager/library/manager.py
@@ -0,0 +1,38 @@
+import sys
+from . import parser
+from . import runner
+from . import reader as rd
+
+
+def uninstall_pkg(host_pkg):
+    print('UNINSTALLING: ', end='')
+    sys.stdout.flush()
+    status = runner.run(host_pkg, "absent")
+    if status != 0:
+        print('FAILED')
+    else:
+        print('OK')
+    return status
+
+
+def install_pkg(host_pkg):
+    print('INSTALLING: ', end='')
+    sys.stdout.flush()
+    status = runner.run(host_pkg, "present")
+    if status != 0:
+        print('FAILED, RUNNING ROLLBACK')
+        uninstall_pkg(host_pkg)
+    else:
+        print('OK')
+    return status
+
+
+def manage_pkg(lines, uninstall, verbose):
+    reader = rd.Reader(lines)
+    host_pkg = parser.parse(reader)
+    print(host_pkg)
+    runner.set_verbose(verbose)
+    if uninstall:
+        return uninstall_pkg(host_pkg)
+    else:
+        return install_pkg(host_pkg)
---
+++ ansible/plugins/modules/pkg_manager/library/parser.py
@@ -0,0 +1,99 @@
+import copy
+
+
+pkg_managers = set(['apt', 'dnf', 'gem', 'pacman', 'pamac', 'pip', 'yay', 'source'])
+
+
+def parse_default(pkg_contaiter, pkg_list):
+    pkg_contaiter += pkg_list
+
+
+def parse_source(pkg_contaiter, pkg_list):
+    num_elems = len(pkg_list)
+    if num_elems % 2 != 0:
+        raise
+    for idx in range(0, num_elems, 2):
+        source = {'url': pkg_list[idx], 'dest': pkg_list[idx + 1]}
+        pkg_contaiter.append(source)
+
+
+parsers = {
+    'apt': parse_default,
+    'dnf': parse_default,
+    'gem': parse_default,
+    'pacman': parse_default,
+    'pamac': parse_default,
+    'pip': parse_default,
+    'yay': parse_source,
+    'source': parse_source
+}
+
+
+def init_host_pkg(host, host_pkg):
+    host_pkg[host] = {}
+    for manager in pkg_managers:
+        host_pkg[host][manager] = []
+
+
+def finalize_group(hosts, pkg, host_pkg):
+    for host in hosts:
+        if host not in host_pkg:
+            init_host_pkg(host, host_pkg)
+        for manager in pkg:
+            if manager not in host_pkg[host]:
+                host_pkg[host][manager] = []
+            host_pkg[host][manager] += copy.deepcopy(pkg[manager])
+    hosts.clear()
+    pkg.clear()
+
+
+def get_chunks(reader):
+    line = reader.get().rstrip()
+    while len(line) == 0:
+        reader.next()
+        line = reader.get().rstrip()
+    return line.split(' ')
+
+
+def collect_hosts(reader):
+    hosts = set()
+    while not reader.end():
+        chunks = get_chunks(reader)
+        if chunks[0] in pkg_managers:
+            break
+        elif len(chunks) > 1:
+            raise
+        else:
+            hosts.add(chunks[0])
+        reader.next()
+    print(hosts)
+    return hosts
+
+
+def collect_pkg(reader):
+    pkg = {}
+    while not reader.end():
+        chunks = get_chunks(reader)
+        if not chunks[0] in pkg_managers:
+            break
+
+        if chunks[0] not in pkg:
+            pkg[chunks[0]] = []
+        parsers[chunks[0]](pkg[chunks[0]], chunks[1:])
+        reader.next()
+
+    print(pkg)
+    return pkg
+
+
+def parse_group(reader, host_pkg):
+    hosts = collect_hosts(reader)
+    pkg = collect_pkg(reader)
+    finalize_group(hosts, pkg, host_pkg)
+
+
+def parse(reader):
+    host_pkg = {}
+    while not reader.end():
+        parse_group(reader, host_pkg)
+    return host_pkg
