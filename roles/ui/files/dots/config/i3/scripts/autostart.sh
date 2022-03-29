a8e27ffe3efda3b7f764ffe196f303dbd77b345d
b08x <rwpannick@gmail.com>
2022-03-29 14:02:05 -0400

    added autostart script for i3

---
+++ roles/ui/files/dots/config/i3/scripts/autostart.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+
+# i3-msg exec /usr/bin/kitty;sleep 1
+
+i3-msg workspace 9, layout splitv
+i3-msg workspace 10, layout splitv
+i3-msg workspace 1
+
+if ! pgrep -x "chrome" > /dev/null
+then
+  i3-msg "exec --no-startup-id  /usr/bin/google-chrome-stable %U";sleep 2
+fi
+
+if ! pgrep -x "zim" > /dev/null
+then
+  i3-msg "exec --no-startup-id zim";sleep 1
+fi
+
+if ! pgrep -x "atom" > /dev/null
+then
+  i3-msg "exec --no-startup-id env ATOM_DISABLE_SHELLING_OUT_FOR_ENVIRONMENT=false /usr/bin/atom";sleep 1
+fi
+
+if ! pgrep -x "patchage" > /dev/null
+then
+  i3-msg "exec --no-startup-id /usr/local/bin/patchage";sleep 1
+fi
+
+if ! pgrep -x "kitty" > /dev/null
+then
+  i3-msg "exec --no-startup-id kitty -1";sleep 1
+fi
+
+xdotool search --onlyvisible --name "Patchage" key ctrl+g
+
+i3-msg [class="^kitty$"], move container to workspace 9
+i3-msg [class="^kitty$"], resize shrink height 400px
+
+i3-msg [class="^Zim$"], move container to workspace 10
+
+i3-msg [class="^Google-chrome$"], move container to workspace 10
+i3-msg [class="^Google-chrome$"], resize shrink height 100px
