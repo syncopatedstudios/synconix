0158e71b677cd2ee89cb8100b8739c366159774c
b08x <rwpannick@gmail.com>
2022-04-12 13:29:23 -0400

    added a msgbox with a sort of a changelog to open on start

--- roles/base/files/dots/.local/bin/autostart.sh
+++ roles/base/files/dots/.local/bin/autostart.sh
@@ -9,9 +9,9 @@ i3-msg workspace 1
 
 i3exec="exec --no-startup-id"
 
-if ! pgrep -x s | grep -e "\-v \-s" > /dev/null
+if ! pgrep -x "s" > /dev/null
 then
-  i3-msg "$i3exec s -v -s";sleep 2
+  i3-msg "exec --no-startup-id 's -s -v'";sleep 1
 fi
 
 if ! pgrep -x "chrome" > /dev/null
@@ -26,30 +26,33 @@ fi
 
 if ! pgrep -x "atom" > /dev/null
 then
-  i3-msg "exec --no-startup-id env ATOM_DISABLE_SHELLING_OUT_FOR_ENVIRONMENT=false /usr/bin/atom";sleep 1
+  i3-msg "exec --no-startup-id env ATOM_DISABLE_SHELLING_OUT_FOR_ENVIRONMENT=false /usr/bin/atom";sleep 5s
 fi
 
 if ! pgrep -x "patchage" > /dev/null
 then
-  i3-msg "exec --no-startup-id /usr/local/bin/patchage";sleep 1
+  i3-msg "exec --no-startup-id patchage";sleep 1
 fi
 
-# if ! pgrep -x "kitty" > /dev/null
-# then
-#   i3-msg "exec --no-startup-id kitty -1 --name 'kitty_init'";sleep 1
-# fi
-
-if ! pgrep -x "s" > /dev/null
+if ! pgrep -x "kitty" > /dev/null
 then
-  i3-msg "exec --no-startup-id 's -s -v'";sleep 1
+  i3-msg "exec --no-startup-id kitty -o allow_remote_control=yes --listen-on unix:/tmp/mykitty --class 'kitty_init' --session $HOME/.config/kitty/start.conf";sleep 1
 fi
 
-xdotool search --onlyvisible --name "Patchage" key ctrl+g
 
-# i3-msg [class="^kitty_init$"], move container to workspace 9
+i3-msg [class='kitty_init'], move container to output HDMI1
 # i3-msg [class="^kitty_init$"], resize shrink height 400px
+
+i3-msg [class="^Google-chrome$"], move container to output HDMI1
+# i3-msg [class="^Google-chrome$"], resize shrink height 100px
 
 i3-msg [class="^Zim$"], move container to workspace 10
 
-i3-msg [class="^Google-chrome$"], move container to workspace 10
-i3-msg [class="^Google-chrome$"], resize shrink height 100px
+i3-msg [class="^Patchage$"], move container to workspace 10
+
+i3-msg workspace 10
+xdotool search --onlyvisible --name "Patchage" key ctrl+g
+
+i3-msg "exec --no-startup-id kitty --class 'kitty_dialog' $HOME/.local/bin/autostart_dialog.sh"
+
+i3-msg workspace 1
---
+++ roles/base/files/dots/.local/bin/autostart_dialog.sh
@@ -0,0 +1,5 @@
+#/usr/bin/env sh
+
+dialog --clear \
+  --title 'New as of 4.12.22' \
+  --msgbox " use 'hg search_term' to search within files using rga\n\n Something else. Given that it's a stupid ass decision, I choose to ignore" 18 80
