39b3557d06ac5d76db7f88fba47b677899363e79
b08x <rwpannick@gmail.com>
2022-04-12 16:46:59 -0400

    set i3 key binding for autostart_dialog

--- roles/base/files/dots/.local/bin/autostart_dialog.sh
+++ roles/base/files/dots/.local/bin/autostart_dialog.sh
@@ -2,4 +2,17 @@
 
 dialog --clear \
   --title 'New as of 4.12.22' \
-  --msgbox " use 'hg search_term' to search within files using rga\n\n Something else. Given that it's a stupid ass decision, I choose to ignore" 18 80
+  --msgbox \
+"# Use kitty links with rga to search and edit\n\n
+---------------------\n \
+
+> hg 'search term'\n \
+
+---------------------\n\n
+# Extract portions of a video file with mpv\n\n
+
+mpv excerpt:\n \
+'b' to begin\n \
+'n' to mark the end\n \
+'x' to write the file\n \
+'L' to toggle looping" 18 80
