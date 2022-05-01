03988b0da8b0eac858873d5d0d1d6aee7e4113e4
b08x <rwpannick@gmail.com>
2022-05-01 07:41:02 -0400

    added backup module to polybar

---
+++ ansible/roles/base/files/dots/.config/polybar/scripts/backup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+if ! pgrep -x "rsync" > /dev/null; then
+  notify-send -u normal "starting backup"
+  osync.sh /etc/osync/sync_crambot_to_bender.conf --silent --summary --verbose --stats
+else
+  notify-send -u normal "backup already happening"
+fi
+
+if [ "$?" -ne 0 ]; then
+  notify-send -u normal "backup failed"
+else
+  notify-send -u normal "backup did not fail"
+fi
