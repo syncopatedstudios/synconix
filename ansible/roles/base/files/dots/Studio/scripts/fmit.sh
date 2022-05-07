08eb1cb610f64a6ee0dab18ab1595e7e9d27de5c
b08x <rwpannick@gmail.com>
2022-05-06 22:50:14 -0400

    updated sonic init settings, added set_levels function in fmit script, set midi_kill bindcode i3

--- ansible/roles/base/files/dots/Studio/scripts/fmit.sh
+++ ansible/roles/base/files/dots/Studio/scripts/fmit.sh
@@ -2,10 +2,16 @@
 
 CAPTURE="Built-in Audio Analog Stereo:capture_FL"
 
-amixer set 'Capture' 9%
-amixer -D hw:0 set 'Capture' 78%
-amixer -D hw:0 set 'Internal Mic Boost' 67%
+set_mic_levels()
+{
+  systemd-cat -t "fmit" amixer set 'Capture' 9%
+  systemd-cat -t "fmit" amixer -D hw:0 set 'Capture' 78%
+  systemd-cat -t "fmit" amixer -D hw:0 set 'Internal Mic Boost' 67%
+  return 0
+}
+
 
 if ! pgrep -x "fmit" > /dev/null; then
+  set_mic_levels
   swallow -d fmit
 fi
