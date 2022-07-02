31267efbe55ebb0f9fc41c8683cf8a325cf93ccd
b08x <rwpannick@gmail.com>
2022-07-02 10:28:48 -0400

    removed refs to rvm

--- ansible/roles/base/files/dots/.zsh/configs/post/path.zsh
+++ ansible/roles/base/files/dots/.zsh/configs/post/path.zsh
@@ -6,11 +6,10 @@ PATH="$PATH:/opt/soundbot/robotstuff/bin"
 PATH="$PATH:$HOME/.cargo/bin"
 PATH="$PATH:/opt/sonic-pi/bin"
 PATH="$PATH:/opt/sononym"
+PATH="$PATH:/opt/atom"
 
 PATH="$PATH:$JAVA_HOME/bin"
 PATH="$PATH:$SOUNDBOT/bin"
-PATH="$PATH:/usr/lib/surfraw"
 PATH="$PATH:$HOME/.local/share/gem/ruby/3.0.0/bin"
-PATH="$PATH:$HOME/.rvm/bin"
 
 export -U PATH
